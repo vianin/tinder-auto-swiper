ea33a2dd3ff816ef8f678ad1a045441ffcdfda1d
Vishal Anand <vianin@users.noreply.github.com>
2019-11-03 07:52:19 +0530

    added random dislike feature

--- auto-swiper.js
+++ auto-swiper.js
@@ -4,14 +4,29 @@ function swipeRight() {
   likeButton.click();
 }
 
+function swipeLeft() {
+  var className = 'recsGamepad__button Start(15px)';
+  var dislikeButton = document.getElementsByClassName(className)[0];
+  dislikeButton.click();
+}
+
 function getRandomDelay() {
   return Math.round(Math.random() * (2000 - 500)) + 500;
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 (function autoSwiper() {
   var delay = getRandomDelay();
+  var choice = getRandomInt(2);
   setTimeout(function () {
-    swipeRight();
+    if (choice == 1) {
+      swipeRight();
+    } else {
+      swipeLeft();
+    }
     autoSwiper();
   }, delay);
 }());
